73d363fd28c394c8fe2267f8dbcda5dc274611f6
Aharon Achildiev <aharonach@gmail.com>
2021-08-15 23:07:33 +0300

    Add factory and garage classes

---
+++ Ex03.GarageLogic/Garage.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Ex03.GarageLogic
+{
+    class Garage
+    {
+        private readonly Dictionary<string, VehicleInGarage> r_Vehicles;
+
+        public Garage()
+        {
+            r_Vehicles = new Dictionary<string, VehicleInGarage>();
+        }
+
+        public void AddVehicle(string i_Person, string i_Phone, Vehicle i_Vehicle)
+        {
+
+        }
+
+        public eVehicleType GetVehicleType(Vehicle i_Vehicle)
+        {
+            eVehicleType type;
+
+            if (i_Vehicle is Truck)
+            {
+                type = eVehicleType.Truck;
+            } 
+            else
+            {
+                type = eVehicleType.ElectricCar;
+            }
+
+            return type;
+        }
+
+        public enum eVehicleType
+        {
+            ElectricMotorcycle,
+            ElectricCar,
+            FuelCar,
+            FuelMotorcycle,
+            Truck
+        }
+    }
+}
---
+++ Ex03.GarageLogic/VehicleInGarage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    class VehicleInGarage
+    {
+        private string m_PersonName;
+        private string m_Phone;
+        private eVehicleStatus m_Status;
+        private Vehicle m_Vehicle;
+        private Garage.eVehicleType m_Type;
+
+        public VehicleInGarage(string i_PersonName, string i_Phone, eVehicleStatus i_Status, Vehicle i_Vehicle)
+        {
+
+        }
+
+        public string PersonName
+        {
+            get { return m_PersonName; }
+            set { m_PersonName = value; }
+        }
+
+        public string Phone
+        {
+            get { return m_Phone; }
+            set { m_Phone = value; }
+        }
+
+        public eVehicleStatus Status
+        {
+            get { return m_Status; }
+            set { m_Status = value; }
+        }
+
+        public Vehicle Vehicle
+        {
+            get { return m_Vehicle; }
+            private set { m_Vehicle = value; }
+        }
+
+        public Garage.eVehicleType Type
+        {
+            get { return m_Type; }
+        }
+        
+        public enum eVehicleStatus
+        {
+            InRepair,
+            Fixed,
+            Payed
+        }
+    }
+}
